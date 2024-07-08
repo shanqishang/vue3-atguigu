876813c4491b2003856242e97a308c7e2fc29799
单其尚 <8615081+wwwhao123521@user.noreply.gitee.com>
2024-07-08 11:59:43 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h2>11</h2>
+    <a>111</a>
+    <h3>{{ a }}</h3>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+var a = ref(1)
+</script>
+
+<style scoped lang="scss">
+#app {
+  width: 100vw;
+
+  height: 100vh;
+}
+</style>
