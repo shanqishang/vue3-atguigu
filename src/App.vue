460f334ce9461dfc3b244f4bad35deaa61ca3d43
单其尚 <8615081+wwwhao123521@user.noreply.gitee.com>
2024-07-08 12:08:15 +0800

    测试husky

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,22 @@
 <template>
   <div id="app">
-        <h2>11</h2>
-          <a>111</a>
-      <h3>{{ a }}</h3>
-        <h4>83953959</h4>
+    <h2>11</h2>
+    <a>111</a>
+    <h3>{{ a }}</h3>
+    <h4>83953959</h4>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from 'vue'
 
-var     a = ref(1);
+var a = ref(1)
 </script>
 
 <style scoped lang="scss">
 #app {
   width: 100vw;
 
-
-
-  
   height: 100vh;
 }
 </style>
